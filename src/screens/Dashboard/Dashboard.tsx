6e52ec75ac82dae5689ab40c85aa41919a55e485
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-22 12:57:04 +0200

    added <> main screens + types

---
+++ src/screens/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {DashboardProps} from './types';
+import React from 'react';
+
+const Dashboard = (props: DashboardProps) => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {LoginProps} from './types';
+import React from 'react';
+
+const Login = (props: LoginProps) => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({});
