3ba7d8f56119850227a1ba9c8a2e13846c53c233
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-22 16:38:18 +0200

    added <> list footer component

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,28 +1,48 @@
-import {StyleSheet, Text, View, Alert, ScrollView} from 'react-native';
-import {DashboardProps} from './types';
-import {SafeAreaView} from 'react-native';
 import {
-  storeFilteredArticles,
+  View,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  Alert,
+  Text,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
   storeArticles,
+  storeFilteredArticles,
 } from '../../features/article/articlesSlice';
-import {FlatList} from 'react-native'; //magic sauce
 import {Article} from '../../features/article/types';
 import {useAppDispatch} from '../../app/rtkHooks';
 import {getArticles} from '../../services/articles';
-import React, {useState, useEffect} from 'react';
 import DashArticleCard from '../../components/DashArticleCard';
+import {themeColors} from '../../helpers/themeColors';
+import {DashboardProps} from './types';
+import {ActivityIndicator} from 'react-native-paper';
 
-const Dashboard = (props: DashboardProps) => {
+const Dashboard = ({
+  searchBaseValue = ``,
+  pageBaseValue = 0,
+  loadingBaseValue = false,
+  articlesListBaseValue = [],
+  searchedArticlesBaseValue = [],
+  dataFoundBaseValue = false,
+}: DashboardProps): JSX.Element => {
   const dispatch = useAppDispatch();
-  const [search, setSearch] = useState<string>(``);
-  const [page, setPage] = useState<number>(0);
-  const [loading, setIsLoading] = useState<boolean>(false);
+  const [search, setSearch] = useState(searchBaseValue);
+  const [page, setPage] = useState(pageBaseValue);
+  const [loading, setIsLoading] = useState(loadingBaseValue);
   const [articleError, setArticleError] = useState<boolean>(false);
-  const [dataFound, setDataFound] = useState<boolean>(false);
-  const [articlesList, setArticlesList] = useState<Article[]>([]);
-  const [searchedArticles, setSearchedArticles] = useState<Article[]>([]);
+  const [dataFound, setDataFound] = useState(dataFoundBaseValue);
+  const [articlesList, setArticlesList] = useState<Article[]>(
+    articlesListBaseValue,
+  );
+  const [searchedArticles, setSearchedArticles] = useState<Article[]>(
+    searchedArticlesBaseValue,
+  );
+
   const loadArticles = async (): Promise<void> => {
     if (page == 0) {
+      setIsLoading(true);
     }
     getArticles(page)
       .then(async (response: any) => {
@@ -31,7 +51,6 @@ const Dashboard = (props: DashboardProps) => {
         }
         setArticlesList(prev => prev.concat(response.response.docs));
         dispatch(storeArticles(response.response.docs));
-        console.log(response.response.docs);
       })
       .catch(error => {
         setArticleError(true);
@@ -49,6 +68,10 @@ const Dashboard = (props: DashboardProps) => {
       })
       .finally(() => setIsLoading(false));
   };
+
+  useEffect(() => {
+    loadArticles();
+  }, [page]);
 
   const searchArticles = (): void => {
     if (search) {
@@ -71,38 +94,82 @@ const Dashboard = (props: DashboardProps) => {
     searchArticles();
   }, [search]);
 
-  return (
-    <View>
-      <Text style={{color: 'black'}}>Hiiiii</Text>
-      <FlatList
-        data={search ? searchedArticles : articlesList}
-        keyExtractor={item => `${item._id}+${Math.random() * 8798789}`}
-        renderItem={({item}) => (
-          <DashArticleCard
-            headline={
-              item?.headline.main ? item.headline.main : 'Title unavailable'
+  const handleOnEndReached = (): void => {
+    if (!search) {
+      setPage(page + 1);
+    }
+  };
+
+  return loading && !articleError ? (
+    <Text style={{color: 'black'}}>Loading...</Text>
+  ) : (
+    <SafeAreaView>
+      <View
+        style={{
+          backgroundColor: themeColors.darkgray,
+          ...styles.maxWidth,
+        }}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={search ? searchedArticles : articlesList}
+          keyExtractor={item => `${item._id}+${Math.random() * 8798789}`}
+          onEndReachedThreshold={0}
+          onEndReached={async ({distanceFromEnd}) => {
+            if (distanceFromEnd < 0) {
+              return;
             }
-            author={
-              !item?.byline?.original || item.byline.original.includes('None')
-                ? 'Unknown author'
-                : item.byline.original
-            }
-            leadParagraph={
-              item?.lead_paragraph.includes('{') ||
-              item?.lead_paragraph.includes('}') ||
-              !item?.lead_paragraph
-                ? 'To see this article, please hold the card'
-                : item?.lead_paragraph
-            }
-            url={item?.web_url ? item?.web_url : null}
-            section={item?.section_name ? item.section_name : 'No section'}
-          />
-        )}
-      />
-    </View>
+            handleOnEndReached();
+          }}
+          renderItem={({item}) => (
+            <DashArticleCard
+              headline={
+                item?.headline.main ? item.headline.main : 'Title unavailable'
+              }
+              author={
+                !item?.byline?.original || item.byline.original.includes('None')
+                  ? 'Unknown author'
+                  : item.byline.original
+              }
+              leadParagraph={
+                item?.lead_paragraph.includes('{') ||
+                item?.lead_paragraph.includes('}') ||
+                !item?.lead_paragraph
+                  ? 'To see this article, please hold the card'
+                  : item?.lead_paragraph
+              }
+              url={item?.web_url ? item?.web_url : null}
+              section={item?.section_name ? item.section_name : 'No section'}
+            />
+          )}
+          ListFooterComponent={
+            !dataFound ? (
+              <ActivityIndicator
+                size="small"
+                color={themeColors.red}
+                style={{marginVertical: 10}}
+              />
+            ) : null
+          }
+          // ListFooterComponent={
+          //   !dataFound && !search ? (
+          //     <ActivityIndicator
+          //       size="small"
+          //       color={themeColors.red}
+          //       style={{marginVertical: 10}}
+          //     />
+          //   ) : null
+          // }
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Dashboard;
+const styles = StyleSheet.create({
+  maxWidth: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Dashboard;
