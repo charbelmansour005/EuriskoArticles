a7c3c82999e88ce6c35c8cff65c2c630860562fd
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-23 09:45:07 +0200

    added <> toast notifications

---
+++ src/features/language/types.ts
@@ -0,0 +1,3 @@
+export type LanguageState = {
+  english: boolean;
+};
---
+++ src/helpers/Toasts.ts
@@ -0,0 +1,17 @@
+import {z} from 'zod';
+
+const DurationSchema = z.object({
+  SHORT: z.number(),
+  MEDIUM: z.number(),
+  LONG: z.number(),
+});
+
+type Duration = z.infer<typeof DurationSchema>;
+
+export const Durations: Duration = {
+  SHORT: 2000,
+  MEDIUM: 4000,
+  LONG: 7000,
+};
+
+// example using zod
