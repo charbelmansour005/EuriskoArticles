5d35f62d9d8115dba2e8ccb2c6533e3729d7204a
charbelmansour005 <charbelmansour005@gmail.com>
2022-12-23 20:11:14 +0200

    added <> about content

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -1,5 +1,4 @@
-import {StyleSheet, Text, View} from 'react-native';
-import {TouchableRipple} from 'react-native-paper';
+import {FlatList, StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
 import LottieView from 'lottie-react-native';
 import {themeColors} from '../../helpers/themeColors';
@@ -7,6 +6,19 @@ import {themeColors} from '../../helpers/themeColors';
 type Props = {};
 
 const About = ({navigation}: any): JSX.Element => {
+  const data = [
+    {key: 'react-native-toast-notifications'},
+    {key: '@react-navigation/native-stack'},
+    {key: '@react-navigation/bottom-tabs'},
+    {key: 'react-native-screens'},
+    {key: '@reduxjs/toolkit'},
+    {key: 'react-native-paper'},
+    {key: 'lottie-react-native'},
+    {key: 'formik'},
+    {key: 'yup'},
+    {key: 'zod'},
+  ];
+
   return (
     <View>
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
@@ -28,6 +40,20 @@ const About = ({navigation}: any): JSX.Element => {
             marginTop: 20,
           }}></View>
       </View>
+      <View style={styles.container}>
+        {data?.map(item => (
+          <Text
+            style={{
+              color: themeColors.skyblue,
+              textAlign: 'center',
+              marginVertical: 5,
+              fontWeight: 'bold',
+            }}
+            key={Math.random()}>
+            {item.key}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 };
@@ -48,5 +74,19 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     color: 'white',
     fontWeight: 'bold',
+  },
+  container: {
+    padding: 22,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 20,
+    borderRadius: 20,
+    backgroundColor: themeColors.transparentGray,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: themeColors.skyblue,
   },
 });
